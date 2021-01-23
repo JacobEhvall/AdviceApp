c9e083384dd0a8ee3fd578579f67424d2adceded
JacobEhvall <jacobehnvallgmail.com>
2021-01-24 00:43:35 +0100

    Added som design, possible to change view.

--- app/src/main/java/com/example/adviceapp/LoginPage.kt
+++ app/src/main/java/com/example/adviceapp/LoginPage.kt
@@ -7,5 +7,11 @@ class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+
+        //Firebase data ska skickas med olika intents som ska skickas vidare till olika sidor
+        // Även att det anvöndaren har skrivit ska komma till profilsidan!
+
+        // Även om det går att få in en bild på en glödlampa
     }
 }
